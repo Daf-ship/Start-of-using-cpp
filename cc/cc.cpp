b26263427b5eed177142f9cecc1963919b9942af
GAMING <GAMING@DESKTOP-3EUSUM2>
2024-07-27 14:07:09 +0100

    Add project files.

---
+++ cc/cc.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "**********************************" << endl;
+	cout << "Name :" << "Alaa\n";
+	cout << "Age :" << "20yr " << endl;
+	cout << "City :" << "Oum El bouaghi\n";
+	cout << "Country :" << "Algeria\n";
+	cout << "Monthly Salary :";
+	short int salary = 500;
+	cout << salary << endl;
+	cout << "yearly Salary :";
+	short int year = 12;
+	cout << salary * year <<endl;
+	cout << "Gender :";
+	char  Male = 'M', Male2 = 'F';
+	cout << Male << endl;
+	cout << "Married :";
+	bool Yes = true, No = false;
+	cout << No << endl;
+	cout << "**********************************" << endl;
+	cout << "I know i will take some time to practise but i will achive my goal" << endl;
+	unsigned short  int tweny = 20 , thery = 30 , ten = 10 ;
+	cout << tweny << "+" << endl;
+	cout << thery << "+" << endl;
+	cout << ten << endl;
+	cout << "____________________" << endl;
+	cout << "Total =";
+	cout << tweny + thery + ten << endl; 
+
+	cout << "\a";
+	cout << "Dear Sir\\Madam" << endl;
+	cout << "How Are You \n";
+	cout << "My Name is \"Alaa Daf\" , nice to meet you." << endl;
+int MyAge = 19 ; // When u put int that mean this variabel its alrady identify for the IDE 
+cout << MyAge << "yr Befor i born" << endl;
+	cout << "Hbib\tAmira\tAlaa\n";
+	cout << "Nada\tFiras\tMaya\n";
+	cout << "Adam\tAdam\tAdam" <<endl ;
+	float Result = 15;
+		cout << "Your result Is " << Result << " Congration" << endl;
+		double Year = 15.3; 
+		cout << "your Full year Result is " << Year << "Congration" << endl;
+		char Fatlatina = 'A', char2 = 'L', char3 = 'A', char4 = 'A';
+			cout << Fatlatina , char2 , char3, char4  ;
+			
+			cout << "+" << endl ;
+			string name = "Daf Alaa";
+				cout << name;
+				cout << "will be my future name" <<endl ;
+				bool Yeah = false;
+					cout << "did U will leave Your contry";
+					cout << Yeah  << endl ;
+					cout << "The reson is";
+					cout << "\"" << "Knowlege for sure" << "\"" << endl;
+
+			
+					int x = 4;
+					int y = 12;
+					int sum = x + y;
+					cout << sum <<endl  ;
+					cout << x + y <<endl;
+
+
+
+
+
+
+        
+
+	return 0;
+
+}
